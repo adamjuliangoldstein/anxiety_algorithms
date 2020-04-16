bbde410e9ef772f4b95f1976b29fe797b9a65a57
Adam Goldstein <adam@adams-mbp-2.lan>
2020-04-16 15:48:19 -0700

    Added inverse cdf for getting weighted random numbers

--- animation.py
+++ animation.py
@@ -20,9 +20,15 @@ def get_distribution_f(a):
     b = 1 - (a / 2)
     return lambda T: a*T + b
 
+def get_inverse_distribution_cdf(a):
+    # f(T) = a*T + 1 - (a/2)
+    # cdf(T) = integral(f(T)) = (a/2)*T^2 + (1 - (a/2))*T
+    # cdf^-1(x):
+    return lambda x: (1 / (2*a))*((a - 2.0) + np.sqrt(a**2 + 8*a*x - 4*a + 4))
 
 a = random.uniform(MIN_A, MAX_A)
 pdf = get_distribution_f(a)
+inv_cdf = get_inverse_distribution_cdf(a)
 
 X = np.linspace(0, 1, num = 100)
 Y_dist = pdf(X)
@@ -37,6 +43,13 @@ max_y = max(get_distribution_f(MIN_A)(0),
             get_distribution_f(MIN_A)(1),
             get_distribution_f(MAX_A)(0),
             get_distribution_f(MAX_A)(1))
+            
+# q = np.random.rand(10000)
+# results = [inv_cdf(i) for i in q]
+# print("a = " + str(a))
+# res = np.mean(results)
+# expected = (a + 6.0)/12.0
+# print(res, expected, res/expected)
 axes.set_ylim([0, max_y])
 plt.plot(X, Y_dist)
 plt.axvline(x=0.91, linewidth=4, color='k')
